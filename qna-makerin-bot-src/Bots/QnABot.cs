2ad85ac4f33875ac61744c684b652dc6df632f25
wbinterns <51494903+wbinterns@users.noreply.github.com>
2019-06-10 13:51:00 -0500

    added all files

---
+++ qna-makerin-bot-src/Bots/QnABot.cs
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class QnABot : ActivityHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<QnABot> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public QnABot(IConfiguration configuration, ILogger<QnABot> logger, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+
+            var qnaMaker = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = _configuration["QnAKnowledgebaseId"],
+                EndpointKey = _configuration["QnAAuthKey"],
+                Host = GetHostname()
+            },
+            null,
+            httpClient);
+
+            _logger.LogInformation("Calling QnA Maker");
+
+            // The actual call to the QnA Maker service.
+            var response = await qnaMaker.GetAnswersAsync(turnContext);
+            if (response != null && response.Length > 0)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
+            }
+        }
+
+        private string GetHostname()
+        {
+            var hostname = _configuration["QnAEndpointHostName"];
+            if (!hostname.StartsWith("https://"))
+            {
+                hostname = string.Concat("https://", hostname);
+            }
+
+            if (!hostname.EndsWith("/qnamaker"))
+            {
+                hostname = string.Concat(hostname, "/qnamaker");
+            }
+
+            return hostname;
+           
+        }
+        //trying to make buttons happen 
+        private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+{
+    var reply = turnContext.Activity.CreateReply("What is your favorite color?");
+    reply.SuggestedActions = new SuggestedActions()
+    {
+        Actions = new List<CardAction>()
+        {
+            new CardAction() { Title = "Red", Type = ActionTypes.ImBack, Value = "Red" },
+            new CardAction() { Title = "Yellow", Type = ActionTypes.ImBack, Value = "Yellow" },
+            new CardAction() { Title = "Blue", Type = ActionTypes.ImBack, Value = "Blue" },
+        },
+    };
+    await turnContext.SendActivityAsync(reply, cancellationToken);
+}
+    }
+}
---
+++ qna-makerin-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qna-makerin-bot-Web-Deploy.pubxml -p:Password=m09Z1qkpWquoirMxocXewhedr3NhyyeHG1raKhcwBQziBYXSMbNMYwlyBDmz
+
